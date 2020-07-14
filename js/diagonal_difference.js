9ccf650d07fef2c9078cf58736ea4a988b5fb4e1
Мухамеджанова Эльмира <timra.work@gmail.com>
2020-07-14 15:36:49 +0500

    Array diagonal-differense done

--- js/diagonal_difference.js
+++ js/diagonal_difference.js
@@ -4,26 +4,24 @@
 // The left-to-right diagonal = 1 + 5 + 9.
 // The right to left diagonal = 3 + 5 + 9.
 // Their absolute difference is = | 15 - 17 | = 2.
-
 // ar = [ [1, 2, 3], [4, 5, 6], [9, 8, 9] ]
 
 'use strict';
 (function () {
   function diagonalDifference(arr) {
-    let result = [];
-    for (let i in arr) {
-      console.log('arr[i]', arr[i]);
-      for(let j in arr[i]){
-        result = arr[j];
-        console.log('arr[j]',j);
-      }
-      // result = arr[i] > b[i] ? ratingA + 1 : ratingA;
-    }
-    return result;
-  }
-  // document.body.insertAdjacentText('afterbegin', compareTriplets([ [1, 2, 3], [4, 5, 6], [9, 8, 9] ]));
-  document.body.insertAdjacentText('afterbegin', diagonalDifference([ [1, 2, 3], [4, 5, 6], [9, 8, 9] ]));
-})();
+    let sum1 = 0;
+    let sum2 = 0;
 
+    for (let i = 0; i < arr.length; i++) {
+      sum1 += arr[i][i];
+      sum2 += arr[i][arr.length - i - 1];
+    }
+
+    return Math.abs(sum1 - sum2);
+  }
+
+  document.body.insertAdjacentText(`afterbegin`, diagonalDifference([[1, 2, 3], [4, 5, 6], [9, 8, 9]]));
+
+})();
 
 
